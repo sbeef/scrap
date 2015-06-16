029602fd992682e247aa35a54c3a39e7547b8bf3
Stanley Beef <stanleybeef@gmail.com>
2015-06-15 23:21:19 -0600

    generates a valid html page

--- scrap.py
+++ scrap.py
@@ -1,4 +1,4 @@
-import time
+import datetime
 
 # indentation stuff
 
@@ -26,12 +26,14 @@ CONTENT_INDENTS = generate_indents(CONTENT_INDENT_LEVEL)
 HEADING_ALIGNMENT = "CENTER"
 HEADING_TYPE      = "H1"
 
+CONTENT_ALIGNMENT = "CENTER"
+
 # templates
 OPEN_HTML     = "%s<HTML>\n" % (HTML_INDENTS)
 CLOSE_HTML    = "%s</HTML>"  % (HTML_INDENTS)
 
 OPEN_HEADER   = "%s<HEAD>\n%s<TITLE>"   % (HEAD_INDENTS, TITLE_INDENTS)
-CLOSE_HEADER  = "</TITLE>\n%s<\HEAD>\n" % (HEAD_INDENTS)
+CLOSE_HEADER  = "</TITLE>\n%s</HEAD>\n" % (HEAD_INDENTS)
 
 OPEN_BODY     = "%s<BODY>\n"  % (BODY_INDENTS)
 CLOSE_BODY    = "%s</BODY>\n" % (BODY_INDENTS)
@@ -39,7 +41,7 @@ CLOSE_BODY    = "%s</BODY>\n" % (BODY_INDENTS)
 OPEN_TITLE    = "%s<%s><%s>"   % (HEADING_INDENTS, HEADING_ALIGNMENT, HEADING_TYPE)
 CLOSE_TITLE   = "</%s></%s>\n" % (HEADING_TYPE, HEADING_ALIGNMENT)
 
-OPEN_CONTENT  = "%s<BR><%s><img src=\"" % (CONTNET_INDENTS, CONTENT_ALIGNMNET)
+OPEN_CONTENT  = "%s<BR/><%s><img src=\"" % (CONTENT_INDENTS, CONTENT_ALIGNMENT)
 CLOSE_CONTENT = "\"/></%s>\n"           % (CONTENT_ALIGNMENT)
 
 
@@ -55,12 +57,15 @@ def generate_content(content):
   return "%s%s%s" % (OPEN_CONTENT, content, CLOSE_CONTENT)
   
 def paste(content, date_string):
-  date = time.strptime(date_string, "%d/%m/%y")
-  post_name = date.strftime("%d-%m-%Y")
+  date = datetime.datetime.strptime(date_string, "%m/%d/%y")
+  post_name = date.strftime("%m-%d-%Y")
   post_file_name = post_name + ".html"
   post_file = open(post_file_name, 'w')
   post_file.write(OPEN_HTML)
   post_file.write(generate_header(post_name))
   post_file.write(OPEN_BODY)
   post_file.write(generate_title(post_name))
-
+  post_file.write(generate_content(content))
+  post_file.write(CLOSE_BODY)
+  post_file.write(CLOSE_HTML)
+  post_file.close()
